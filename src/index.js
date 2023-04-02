818ebc19e5c5fc5419e1de8b26f982320ed1b079
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-02 15:12:21 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 /* 
-
 To-do list; objects that are created 
 using factories or constructors/classes
-
 properties should have at least title, description, dueDate, 
 and priority.
-
 might be good to include notes and checklist. 
     - should have projects/seperate list of todos
     - on open, default project shows with all their todos
     - users should be able to create new projects and
     chose which project their todos go into.
-
 Seperate application logic from DOM-related stuff
     - creating new todos
     - setting todos as complete
     - changing todo priority
-
 UI should be able to:
     - view all projects
     - view all todos in each project (ex. Title, duedate, and priorities)
