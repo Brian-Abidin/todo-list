ad4c4e285e9bfef19fcb5d78ad42494ac401c99d
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-04 14:45:59 -0400

    updated functions to add the project created to the allProjects array

--- src/index.js
+++ src/index.js
@@ -17,21 +17,21 @@ UI should be able to:
     - view all todos in each project (ex. Title, duedate, and priorities)
     - expand a single todo to see/edit its details
     - delete a todo
-    
-*/
 
-/*
 - Write an array 
-
 */
+
+const allProjects = [];
 
 for (let i = Array.length - 1; i < Array.length; i += 1) {
   // code here adds task to array
 }
 
+// RemoveProject Function
+
 // create object
-function addProject(name, desc, urgency, time) {
-  return {
+function createProject(name, desc, urgency, time) {
+  const project = {
     type: "project",
     name,
     description: desc,
@@ -40,58 +40,64 @@ function addProject(name, desc, urgency, time) {
     tags: [],
     tasks: []
   };
+  allProjects.push(project);
 }
+
+// on click creates this object and pushes it into the array.
+createProject("newtask", "plans out a plan", "green", "11:59");
+
+console.log(allProjects[0]);
+console.log(allProjects);
 
 // add a task to project
 // - add a task w/ description & due date, priority
 // - add a checklist w/ due date, priority
 
-function addTask(obj, title, desc) {
+// checks which project you are on and adds task from the project based on the array order
+function addTask(title, desc) {
   const task = {
     type: "task",
     title,
     description: desc
   };
-  obj.tasks.push(task);
+  allProjects[0].tasks.push(task);
 }
 
-function addChecklist(obj, title) {
+function addChecklist(title) {
   const checkList = {
     type: "checklist",
     title
   };
-  obj.tasks.push(checkList);
+  allProjects[0].tasks.push(checkList);
   // DOM element function that adds the checklist box
 }
 
-function addTag(obj, title) {
+function addTag(title) {
   const tag = {
     type: "tag",
     title
   };
-  obj.tags.push(tag);
+  allProjects[0].tags.push(tag);
 }
 
 // form that can be written
-const newtask = addProject("newtask", "plans out a plan", "green", "11:59");
-addTask(newtask, "Buy a House", "Get enough money to buy an entire house");
-addTask(newtask, "Buy a Car", "Get enough money to buy an entire car");
-addChecklist(newtask, "Open Car");
+// const newtask = addProject("newtask", "plans out a plan", "green", "11:59");
+addTask("Buy a House", "Get enough money to buy an entire house");
+addTask("Buy a Car", "Get enough money to buy an entire car");
+addChecklist("Open Car");
 // Object.defineProperty(newtask, "tag", {
 //   value: "gaming",
 //   writable: false,
 //   enumerable: true
 // });
-addTag(newtask, "work");
+addTag("work");
 
 // addTaskProperty(newtask, "tag", "gaming");
 // addTaskProperty(newtask, "tag", "sports");
 // const objProps = Object.keys(newtask).length;
 
-console.log(Object.keys(newtask).length);
+console.log(Object.keys(allProjects[0]).length);
 
-for (const key in newtask) {
+for (const key in allProjects[0]) {
   console.log(key);
 }
-
-console.log(newtask);
