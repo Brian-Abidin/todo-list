2c8a3e8484a89903fde3d4f8b5d1b4b1da030a8d
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-05 18:45:17 -0400

    added functions folder

---
+++ src/functions/createProject.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("create");
+
+function openForm() {}
+
+function closeForm() {}
+
+button.onclick = function createProject() {
+  this.style.display = "block";
+};
