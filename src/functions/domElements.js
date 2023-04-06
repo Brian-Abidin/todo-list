0d320191fced4101b91d2cb59099113d4655c6d6
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-06 18:21:11 -0400

    added constants

--- src/functions/domElements.js
+++ src/functions/domElements.js
@@ -1,1 +1,15 @@
 // Constants
+
+const wrapper = document.getElementById("wrapper");
+const header = document.getElementById("header");
+const createForm = document.getElementById("createForm");
+const formPopup = document.getElementById("formPopup");
+const formContainer = document.getElementById("formContainer");
+const button_container = document.getElementById("button_container");
+const formClose = document.getElementById("formClose");
+const closeSign = document.getElementById("closeSign");
+const btnSubmit = document.getElementById("btnSubmit");
+const formContent = document.getElementById("formContent");
+const taskName = document.getElementById("taskName");
+const formBottom = document.getElementById("formBottom");
+const 
