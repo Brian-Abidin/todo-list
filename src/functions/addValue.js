7e88f6386fdee516b721ee027b27714e853e3fad
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-07 15:17:16 -0400

    updated function

--- src/functions/addValue.js
+++ src/functions/addValue.js
@@ -1,13 +1,20 @@
-import { low } from "./domElements";
+import { high, low, medium } from "./domElements";
+import createProject from "./createProject";
 
-function addValues() {
+export default function addValues() {
   const taskValue = document.getElementById("task-name").value;
-  const tagValue = document.getElementById("add-tag").value;
+  //   const tagValue = document.getElementById("add-tag").value;
+  let urgency = "";
   const descriptionValue = document.getElementById("description").value;
   const dateValue = document.getElementById("due-date").value;
   const timeValue = document.getElementById("due-time").value;
   if (low.checked) {
-    console.log("hello world");
+    urgency = "low";
+  } else if (medium.checked) {
+    urgency = "medium";
+  } else if (high.checked) {
+    urgency = "high";
   }
-  console.log(taskValue, tagValue, descriptionValue, dateValue, timeValue);
+  console.log(taskValue, descriptionValue, urgency, dateValue, timeValue);
+  createProject(taskValue, descriptionValue, urgency, dateValue, timeValue);
 }
