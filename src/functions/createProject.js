27864f01bd49f64ed15efb86ee5f8c5e331326da
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-07 15:37:39 -0400

    added displayProject function

--- src/functions/createProject.js
+++ src/functions/createProject.js
@@ -1,4 +1,28 @@
-import { allProjects } from "./domElements";
+import { allProjects, contentLeft } from "./domElements";
+
+function displayProject(name, urgency, date) {
+  const showProject = document.createElement("div");
+  const priority = document.createElement("div");
+  const title = document.createElement("div");
+  const dueDate = document.createElement("div");
+  const projectBtn = document.createElement("div");
+
+  showProject.classList.add = "project";
+  priority.classList.add = "project-priority";
+  title.classList.add = "project-title";
+  dueDate.classList.add = "project-duedate";
+  projectBtn.classList.add = "project-button";
+
+  contentLeft.appendChild(showProject);
+  showProject.appendChild(priority);
+  showProject.appendChild(title);
+  showProject.appendChild(dueDate);
+  showProject.appendChild(projectBtn);
+
+  title.textContent = name;
+  dueDate.textContent = date;
+  priority.textContent = urgency;
+}
 
 export default function createProject(name, desc, urgency, date, time) {
   const project = {
@@ -12,5 +36,6 @@ export default function createProject(name, desc, urgency, date, time) {
     tasks: []
   };
   allProjects.push(project);
+  displayProject(project.name, project.priority, project.dueDate);
   console.log(allProjects);
 }
