f57b277aba472a442dba02b23fcdb29994a0093f
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-08 16:55:54 -0400

    added checkDisplay function inside displayProject function

--- src/functions/createProject.js
+++ src/functions/createProject.js
@@ -11,41 +11,6 @@ import {
   rightDescription
 } from "./domElements";
 
-function displayProject(name, urgency, date) {
-  const projectOrder = allProjects.length;
-  const showProject = document.createElement("div");
-  const priority = document.createElement("div");
-  const title = document.createElement("div");
-  const dueDate = document.createElement("div");
-  const projectBtn = document.createElement("button");
-
-  showProject.classList.add("project");
-  priority.classList.add("project-priority");
-  title.classList.add("project-title");
-  dueDate.classList.add("project-duedate");
-  projectBtn.classList.add("project-button");
-
-  if (urgency === "low") {
-    priority.style.backgroundColor = "green";
-  } else if (urgency === "medium") {
-    priority.style.backgroundColor = "orange";
-  } else if (urgency === "high") {
-    priority.style.backgroundColor = "red";
-  }
-
-  showProject.setAttribute("id", projectOrder);
-
-  contentLeft.appendChild(showProject);
-  showProject.appendChild(priority);
-  showProject.appendChild(title);
-  showProject.appendChild(dueDate);
-  showProject.appendChild(projectBtn);
-
-  title.textContent = name;
-  dueDate.textContent = date;
-  projectBtn.textContent = ">";
-}
-
 // function addingTag() {
 //   const tagValue = document.getElementById("add-tag").value;
 //   document.getElementById("add-tag").value = "";
@@ -78,6 +43,59 @@ function displayProjectRight(name, desc, urgency, date, time, tags) {
 
   tagName.textContent = tags;
   removeTag.textContent = "-";
+}
+
+function displayProject(name, urgency, date) {
+  const projectOrder = allProjects.length;
+  const showProject = document.createElement("div");
+  const priority = document.createElement("div");
+  const title = document.createElement("div");
+  const dueDate = document.createElement("div");
+  const projectBtn = document.createElement("button");
+
+  showProject.classList.add("project");
+  priority.classList.add("project-priority");
+  title.classList.add("project-title");
+  dueDate.classList.add("project-duedate");
+  projectBtn.classList.add("project-button");
+
+  if (urgency === "low") {
+    priority.style.backgroundColor = "green";
+  } else if (urgency === "medium") {
+    priority.style.backgroundColor = "orange";
+  } else if (urgency === "high") {
+    priority.style.backgroundColor = "red";
+  }
+
+  showProject.setAttribute("id", projectOrder);
+  projectBtn.setAttribute("id", projectOrder);
+
+  contentLeft.appendChild(showProject);
+  showProject.appendChild(priority);
+  showProject.appendChild(title);
+  showProject.appendChild(dueDate);
+  showProject.appendChild(projectBtn);
+
+  title.textContent = name;
+  dueDate.textContent = date;
+  projectBtn.textContent = ">";
+
+  (function checkDislay() {
+    if (allProjects.length !== 0) {
+      projectBtn.addEventListener("click", (e) => {
+        const thisProject = allProjects[e.target.id - 1];
+        console.log(thisProject);
+        displayProjectRight(
+          thisProject.name,
+          thisProject.description,
+          thisProject.priority,
+          thisProject.date,
+          thisProject.time,
+          thisProject.tags
+        );
+      });
+    }
+  })();
 }
 
 export default function createProject(name, desc, urgency, date, time) {
