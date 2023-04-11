67533dba224642676ffbed85317d127ad2519dc0
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-11 14:06:01 -0400

    moved click eventlistneers to index.js

--- src/functions/openCloseForm.js
+++ src/functions/openCloseForm.js
@@ -1,13 +1,6 @@
-import {
-  btnSubmit,
-  formPopup,
-  formContainer,
-  createForm,
-  formClose
-} from "./domElements";
+import { btnSubmit, formPopup, formContainer } from "./domElements";
 
 export function openForm() {
-  // btnSubmit.type = "submit";
   formPopup.style.display = "block";
   formContainer.style.display = "block";
   console.log(btnSubmit.type);
@@ -17,6 +10,3 @@ export default function closeTheForm() {
   formPopup.style.display = "none";
   formContainer.style.display = "none";
 }
-
-formClose.addEventListener("click", closeTheForm);
-createForm.addEventListener("click", openForm);
--- src/functions/submitForm.js
+++ src/functions/submitForm.js
@@ -2,12 +2,10 @@ import addValues from "./addValue";
 import { form } from "./domElements";
 import closeTheForm from "./openCloseForm";
 
-function submitForm(event) {
+export default function submitForm(event) {
   console.log("This is working");
   event.preventDefault();
   addValues();
   form.reset();
   closeTheForm();
 }
-
-form.addEventListener("submit", submitForm);
