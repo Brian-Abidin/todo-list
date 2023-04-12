7e52c2f07feb9c626aec450203a30e60b0e5bcb2
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 18:26:06 -0400

    removed comments and added projects to webpage on window load

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import fillInfo from "./functions/fillInfo";
 import checkDelete from "./functions/checkDelete";
 import updateProjectInfo from "./functions/updateProjectInfo";
 import welcome from "./functions/welcome";
+import loadProjects from "./functions/loadProjects";
 
 /* 
 To-do list; objects that are created 
@@ -64,44 +65,11 @@ const result = new Date();
 const resultFormatted = format(result, "MM/dd/yyyy");
 console.log(result);
 
-// (function welcomeIcon() {
-//   const currentHour = +format(new Date(), "k");
-//   switch (true) {
-//     case currentHour >= 20 || currentHour < 6:
-//       greeting.textContent = "Good Evening, User";
-//       contentTop.style.backgroundImage =
-//         "linear-gradient(rgb(51, 41, 41), rgb(0, 34, 128))";
-//       weatherIcon.src = "../src/images/moon.png";
-//       console.log("moon");
-//       break;
-//     case currentHour >= 6 && currentHour < 12:
-//       greeting.textContent = "Good Morning, User";
-//       weatherIcon.src = "../src/images/sun.png";
-//       contentTop.style.backgroundImage =
-//         "linear-gradient(rgb(6, 132, 235), rgb(255, 255, 255))";
-//       console.log("sunrise");
-//       break;
-//     case currentHour >= 12 && currentHour < 19:
-//       greeting.textContent = "Good Afternoon, User";
-//       weatherIcon.src = "../src/images/sunrise.png";
-//       contentTop.style.backgroundImage =
-//         "linear-gradient(rgb(255, 174, 0), rgb(2, 175, 255))";
-//       console.log("sun");
-//       break;
-//     case currentHour >= 19 && currentHour < 20:
-//       greeting.textContent = "Good Evening, User";
-//       weatherIcon.src = "../src/images/moon.png";
-//       contentTop.style.backgroundImage =
-//         "linear-gradient(rgb(51, 41, 41), rgb(0, 34, 128))";
-//       console.log("sunset");
-//       break;
-//     default:
-//       console.log("welcomeIcon function broken");
-//   }
-// })();
-
 window.onload = () => {
   welcome();
+  const projects = JSON.parse(localStorage.getItem("projects") || "[]");
+  loadProjects(projects);
+  console.log(projects);
 };
 
 formClose.addEventListener("click", () => {
@@ -130,12 +98,7 @@ rightEdit.addEventListener("click", (e) => {
 
 rightDelete.addEventListener("click", () => {
   checkDelete();
+  window.localStorage.setItem("projects", JSON.stringify(allProjects));
 });
-
-// finishBtn.addEventListener("change", () => {
-//   if (this.checked) {
-//     console.log("checking works!");
-//   }
-// });
 
 console.log(resultFormatted);
