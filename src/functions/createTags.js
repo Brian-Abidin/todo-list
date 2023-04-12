3e2a3bae833818ecc8335a6b9c33b8cc1bf0e997
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 12:36:28 -0400

    tags are no longer in an array

--- src/functions/createTags.js
+++ src/functions/createTags.js
@@ -1,24 +1,22 @@
 import { displayTags } from "./domElements";
 
 export default function createTags(tags) {
-  for (let i = 0; i < tags.length; i += 1) {
-    if (tags[0] === "") {
-      // do nothing
-    } else {
-      const tag = document.createElement("div");
-      const tagName = document.createElement("div");
-      const removeTag = document.createElement("button");
+  if (tags === "") {
+    // do nothing
+  } else {
+    const tag = document.createElement("div");
+    const tagName = document.createElement("div");
+    const removeTag = document.createElement("button");
 
-      tag.classList.add("tag1");
-      tagName.classList.add("tag-name");
-      removeTag.classList.add("remove-tag");
+    tag.classList.add("tag1");
+    tagName.classList.add("tag-name");
+    removeTag.classList.add("remove-tag");
 
-      displayTags.appendChild(tag);
-      tag.appendChild(tagName);
-      tag.appendChild(removeTag);
+    displayTags.appendChild(tag);
+    tag.appendChild(tagName);
+    tag.appendChild(removeTag);
 
-      tagName.textContent = tags[i];
-      removeTag.textContent = "-";
-    }
+    tagName.textContent = tags;
+    removeTag.textContent = "-";
   }
 }
