43e3aa481d98c14ff86bdeadb4e7c0331a71e005
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 11:59:17 -0400

    added window onload event and moved welcomeIcon function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import { format } from "date-fns";
-import { updateProjectInfo } from "./functions/createProject";
 import {
   allProjects,
-  greeting,
-  weatherIcon,
-  contentTop,
   createForm,
   formClose,
   form,
@@ -16,6 +12,8 @@ import closeTheForm, { openForm } from "./functions/openCloseForm";
 import submitForm from "./functions/submitForm";
 import fillInfo from "./functions/fillInfo";
 import checkDelete from "./functions/checkDelete";
+import updateProjectInfo from "./functions/updateProjectInfo";
+import welcome from "./functions/welcome";
 
 /* 
 To-do list; objects that are created 
@@ -66,41 +64,45 @@ const result = new Date();
 const resultFormatted = format(result, "MM/dd/yyyy");
 console.log(result);
 
-(function welcomeIcon() {
-  const currentHour = +format(new Date(), "k");
-  switch (true) {
-    case currentHour >= 20 || currentHour < 6:
-      greeting.textContent = "Good Evening, User";
-      contentTop.style.backgroundImage =
-        "linear-gradient(rgb(51, 41, 41), rgb(0, 34, 128))";
-      weatherIcon.src = "../src/images/moon.png";
-      console.log("moon");
-      break;
-    case currentHour >= 6 && currentHour < 12:
-      greeting.textContent = "Good Morning, User";
-      weatherIcon.src = "../src/images/sun.png";
-      contentTop.style.backgroundImage =
-        "linear-gradient(rgb(6, 132, 235), rgb(255, 255, 255))";
-      console.log("sunrise");
-      break;
-    case currentHour >= 12 && currentHour < 19:
-      greeting.textContent = "Good Afternoon, User";
-      weatherIcon.src = "../src/images/sunrise.png";
-      contentTop.style.backgroundImage =
-        "linear-gradient(rgb(255, 174, 0), rgb(2, 175, 255))";
-      console.log("sun");
-      break;
-    case currentHour >= 19 && currentHour < 20:
-      greeting.textContent = "Good Evening, User";
-      weatherIcon.src = "../src/images/moon.png";
-      contentTop.style.backgroundImage =
-        "linear-gradient(rgb(51, 41, 41), rgb(0, 34, 128))";
-      console.log("sunset");
-      break;
-    default:
-      console.log("welcomeIcon function broken");
-  }
-})();
+// (function welcomeIcon() {
+//   const currentHour = +format(new Date(), "k");
+//   switch (true) {
+//     case currentHour >= 20 || currentHour < 6:
+//       greeting.textContent = "Good Evening, User";
+//       contentTop.style.backgroundImage =
+//         "linear-gradient(rgb(51, 41, 41), rgb(0, 34, 128))";
+//       weatherIcon.src = "../src/images/moon.png";
+//       console.log("moon");
+//       break;
+//     case currentHour >= 6 && currentHour < 12:
+//       greeting.textContent = "Good Morning, User";
+//       weatherIcon.src = "../src/images/sun.png";
+//       contentTop.style.backgroundImage =
+//         "linear-gradient(rgb(6, 132, 235), rgb(255, 255, 255))";
+//       console.log("sunrise");
+//       break;
+//     case currentHour >= 12 && currentHour < 19:
+//       greeting.textContent = "Good Afternoon, User";
+//       weatherIcon.src = "../src/images/sunrise.png";
+//       contentTop.style.backgroundImage =
+//         "linear-gradient(rgb(255, 174, 0), rgb(2, 175, 255))";
+//       console.log("sun");
+//       break;
+//     case currentHour >= 19 && currentHour < 20:
+//       greeting.textContent = "Good Evening, User";
+//       weatherIcon.src = "../src/images/moon.png";
+//       contentTop.style.backgroundImage =
+//         "linear-gradient(rgb(51, 41, 41), rgb(0, 34, 128))";
+//       console.log("sunset");
+//       break;
+//     default:
+//       console.log("welcomeIcon function broken");
+//   }
+// })();
+
+window.onload = () => {
+  welcome();
+};
 
 formClose.addEventListener("click", () => {
   closeTheForm();
