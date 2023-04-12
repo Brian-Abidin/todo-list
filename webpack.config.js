bb9d377e352a7459f3d962a3e4b56083be81933f
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 16:45:28 -0400

    no long code splitting. Now a single entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: {
-    index: {
-      import: "./src/index.js",
-      dependOn: "shared"
-    },
-    addvalue: {
-      import: "./src/functions/addValue.js",
-      dependOn: "shared"
-    },
-    createproject: {
-      import: "./src/functions/createProject.js",
-      dependOn: "shared"
-    },
-    fillinfo: {
-      import: "./src/functions/fillInfo.js",
-      dependOn: "shared"
-    },
-    updateprojectinfo: {
-      import: "./src/functions/updateProjectInfo.js",
-      dependOn: "shared"
-    },
-    welcome: {
-      import: "./src/functions/welcome.js",
-      dependOn: "shared"
-    },
-    shared: "date-fns"
-  },
+  entry: "./src/index.js",
   devtool: "inline-source-map",
   output: {
-    filename: "[name].bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist")
-  },
-  optimization: {
-    runtimeChunk: "single"
   },
   module: {
     rules: [
