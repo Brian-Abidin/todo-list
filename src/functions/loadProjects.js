86224a6a3af9871284b9d56461b3772ce38c2732
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 18:48:03 -0400

    if checkbox is checked on refresh, stays checked

--- src/functions/loadProjects.js
+++ src/functions/loadProjects.js
@@ -2,13 +2,21 @@ import { getProjects } from "./createProject";
 
 export default function loadProjects(projects) {
   for (let i = 0; i < projects.length; i += 1) {
+    const id = i + 1;
     getProjects(
       projects[i].name,
       projects[i].description,
       projects[i].priority,
       projects[i].dueDate,
       projects[i].time,
-      projects[i].tags
+      projects[i].tags,
+      projects[i].done
     );
+    if (projects[i].done === "yes") {
+      const isChecked = document.getElementById(`finish${id}`);
+      isChecked.checked = true;
+      const projectTitle = document.getElementById(`title${id}`);
+      projectTitle.style.textDecoration = "line-through";
+    }
   }
 }
