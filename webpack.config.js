a44fbd2e6de32e17c93b49bb6f81e3d64efd533e
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 13:51:42 -0400

    code split files that depend on the date-fns library and output them seperately

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,41 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: {
+      import: "./src/index.js",
+      dependOn: "shared"
+    },
+    addvalue: {
+      import: "./src/functions/addValue.js",
+      dependOn: "shared"
+    },
+    createproject: {
+      import: "./src/functions/createProject.js",
+      dependOn: "shared"
+    },
+    fillinfo: {
+      import: "./src/functions/fillInfo.js",
+      dependOn: "shared"
+    },
+    updateprojectinfo: {
+      import: "./src/functions/updateProjectInfo.js",
+      dependOn: "shared"
+    },
+    welcome: {
+      import: "./src/functions/welcome.js",
+      dependOn: "shared"
+    },
+    shared: "date-fns"
+  },
   devtool: "inline-source-map",
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    runtimeChunk: "single"
   },
   module: {
     rules: [
