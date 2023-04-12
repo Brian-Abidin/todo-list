799e91e5fa296787834be27a0379ab5a66356301
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 18:48:59 -0400

    added if title has linethrough, on refresh, line stays

--- src/functions/projectDone.js
+++ src/functions/projectDone.js
@@ -1,9 +1,16 @@
+import { allProjects } from "./domElements";
+
 export default function projectDone(id) {
   const idNum = id.slice(-1);
   const projectName = document.getElementById(`title${idNum}`);
+  const projectNum = Number(idNum) - 1;
   if (projectName.style.textDecoration === "line-through") {
     projectName.style.textDecoration = "";
+    allProjects[projectNum].done = "no";
+    window.localStorage.setItem("projects", JSON.stringify(allProjects));
   } else {
     projectName.style.textDecoration = "line-through";
+    allProjects[projectNum].done = "yes";
+    window.localStorage.setItem("projects", JSON.stringify(allProjects));
   }
 }
