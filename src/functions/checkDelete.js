c6acc1ed3629481782f8e6a1d9a41fb00baee806
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 12:35:59 -0400

    spacing

--- src/functions/checkDelete.js
+++ src/functions/checkDelete.js
@@ -1,9 +1,9 @@
-import { allProjects, contentRight } from "./domElements";
+import { allProjects, contentRight, rightDelete } from "./domElements";
 import updateProjectIds from "./updateProjectIds";
 
-export default function checkDelete(e) {
-  console.log(e.target.id);
-  const projectId = Number(e.target.id);
+export default function checkDelete() {
+  console.log(rightDelete.id);
+  const projectId = Number(rightDelete.id);
   const projectIndex = projectId - 1;
   const element = document.getElementById(`project${projectId}`);
   console.log("check Delete", projectIndex);
