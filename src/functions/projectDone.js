3b90f66ea8a168cd05b4e2d9e87924905a044294
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-12 17:38:45 -0400

    updated projectName variable

--- src/functions/projectDone.js
+++ src/functions/projectDone.js
@@ -1,6 +1,6 @@
 export default function projectDone(id) {
   const idNum = id.slice(-1);
-  const projectName = document.getElementById(`project${idNum}`);
+  const projectName = document.getElementById(`title${idNum}`);
   if (projectName.style.textDecoration === "line-through") {
     projectName.style.textDecoration = "";
   } else {
