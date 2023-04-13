97bf833f59e045c5dc96e8818b575dc98197b585
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-13 15:13:08 -0400

    removed css and image webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource"
-      }
-    ]
+    rules: []
   }
 };
