02e32d39f929450f942f3a03f5d8779d0e37c426
Brian Abidin <Brian.abidin1@gmail.com>
2023-04-13 14:43:24 -0400

    removed remove tag section

--- src/functions/createTags.js
+++ src/functions/createTags.js
@@ -6,17 +6,13 @@ export default function createTags(tags) {
   } else {
     const tag = document.createElement("div");
     const tagName = document.createElement("div");
-    const removeTag = document.createElement("button");
 
     tag.classList.add("tag1");
     tagName.classList.add("tag-name");
-    removeTag.classList.add("remove-tag");
 
     displayTags.appendChild(tag);
     tag.appendChild(tagName);
-    tag.appendChild(removeTag);
 
     tagName.textContent = tags;
-    removeTag.textContent = "-";
   }
 }
