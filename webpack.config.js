9b47b18c83da0c102dbc4a1983d4ac12244339b5
Brian Abidin <Brian.abidin1@gmail.com>
2023-05-03 16:01:46 -0400

    added babel as a loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,19 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
   module: {
-    rules: []
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
   }
 };
